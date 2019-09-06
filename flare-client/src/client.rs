484840aa0a861c166392bf1ba15a343ccc06d53a
龚德伟 <gongdewei@szjlc.com>
2019-09-06 18:09:59 +0800

    extract fn handle_connection

--- flare-client/src/client.rs
+++ flare-client/src/client.rs
@@ -6,6 +6,8 @@ use websocket::sync::Server;
 use websocket::OwnedMessage;
 use websocket::sync::sender::Sender;
 use websocket::sender::Writer;
+use websocket::server::upgrade::WsUpgrade;
+use websocket::server::upgrade::sync::Buffer;
 
 pub struct Profiler {
     self_ref: Option<Arc<Mutex<Profiler>>>,
@@ -41,71 +43,69 @@ impl Profiler {
     }
 
     pub fn get_dashboard(&mut self) {
-        (&self.sample_client.as_ref().unwrap()).lock().unwrap().get_dashboard();
-//        if let Some(client) = &self.sample_client {
-//            client.lock().unwrap().get_dashboard();
-//        }
+        self.sample_client.as_ref().unwrap().lock().unwrap().get_dashboard();
     }
 
     fn start_ws_server(&mut self) {
-        if let Some(self_ref) = &self.self_ref {
-            let this_ref = self_ref.clone();
-            let bind_addr = self.analysis_bind_addr.clone();
-            thread::spawn(move || {
-                println!("analysis server binding: {}", bind_addr);
-                let server = Server::bind(bind_addr).unwrap();
-                for request in server.filter_map(Result::ok) {
-                    if !this_ref.lock().unwrap().running {
-                        println!("Shutting down analysis ws server ...");
-                        break;
-                    }
-                    // Spawn a new thread for each connection.
-                    let ws_this_ref = this_ref.clone();
-                    thread::spawn(move || {
-                        let ws_protocol = "flare-ws";
-                        if !request.protocols().contains(&ws_protocol.to_string()) {
-                            request.reject().unwrap();
-                            return;
-                        }
-
-                        let mut client = request.use_protocol(ws_protocol).accept().unwrap();
-
-                        let ip = client.peer_addr().unwrap();
-
-                        println!("Connection from {}", ip);
-
-                        let message = OwnedMessage::Text("Hello".to_string());
-                        client.send_message(&message).unwrap();
-
-                        let (mut receiver, mut sender) = client.split().unwrap();
-
-                        for message in receiver.incoming_messages() {
-                            let message = message.unwrap();
-
-                            match message {
-                                OwnedMessage::Close(_) => {
-                                    let message = OwnedMessage::Close(None);
-                                    sender.send_message(&message).unwrap();
-                                    println!("Client {} disconnected", ip);
-                                    return;
-                                }
-                                OwnedMessage::Ping(ping) => {
-                                    let message = OwnedMessage::Pong(ping);
-                                    sender.send_message(&message).unwrap();
-                                }
-                                OwnedMessage::Text(json) => {
-                                    ws_this_ref.lock().unwrap().handle_request(&mut sender, json);
-                                }
-                                _ => sender.send_message(&message).unwrap(),
-                            }
-                        }
-                    });
+        let self_ref = self.self_ref.as_ref().unwrap().clone();
+        let bind_addr = self.analysis_bind_addr.clone();
+        thread::spawn(move || {
+            println!("analysis server binding: {}", bind_addr);
+            let server = Server::bind(bind_addr).unwrap();
+            for request in server.filter_map(Result::ok) {
+                if !self_ref.lock().unwrap().is_running() {
+                    println!("Shutting down analysis ws server ...");
+                    return;
                 }
-            });
-        }
+                Profiler::handle_connection(self_ref.clone(), request);
+            }
+        });
     }
 
-    fn handle_request(&mut self, sender: &mut Writer<std::net::TcpStream>, json:String) {
+    fn handle_connection(self_ref: Arc<Mutex<Profiler>>, request: WsUpgrade<std::net::TcpStream, core::option::Option<Buffer>>) {
+        // Spawn a new thread for each connection.
+        thread::spawn(move || {
+            let ws_protocol = "flare-ws";
+            if !request.protocols().contains(&ws_protocol.to_string()) {
+                request.reject().unwrap();
+                return;
+            }
+
+            let mut client = request.use_protocol(ws_protocol).accept().unwrap();
+
+            let ip = client.peer_addr().unwrap();
+
+            println!("Connection from {}", ip);
+
+            let message = OwnedMessage::Text("Hello".to_string());
+            client.send_message(&message).unwrap();
+
+            let (mut receiver, mut sender) = client.split().unwrap();
+
+            for message in receiver.incoming_messages() {
+                let message = message.unwrap();
+
+                match message {
+                    OwnedMessage::Close(_) => {
+                        let message = OwnedMessage::Close(None);
+                        sender.send_message(&message).unwrap();
+                        println!("Client {} disconnected", ip);
+                        return;
+                    }
+                    OwnedMessage::Ping(ping) => {
+                        let message = OwnedMessage::Pong(ping);
+                        sender.send_message(&message).unwrap();
+                    }
+                    OwnedMessage::Text(json) => {
+                        Profiler::handle_request(self_ref.clone(), &mut sender, json);
+                    }
+                    _ => sender.send_message(&message).unwrap(),
+                }
+            }
+        });
+    }
+
+    fn handle_request(self_ref: Arc<Mutex<Profiler>>, sender: &mut Writer<std::net::TcpStream>, json:String) {
         println!("recv: {}", json);
         //TODO parse request to json
         let message = OwnedMessage::Text(json);
