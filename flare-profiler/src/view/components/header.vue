1cd19e106487f5844cd35405f3e4f05f557e5adc
徐竹 <xuzhu@szjlc.com>
2019-09-07 10:21:31 +0800

    提交图片以及样式

---
+++ flare-profiler/src/view/components/header.vue
@@ -0,0 +1,21 @@
+<template>
+    <el-page-header @back="goBack" content="详情页面"></el-page-header>
+</template>
+
+<script>
+    export default {
+        name: "header",
+        data:{
+
+        },
+        methods:{
+            goBack() {
+                console.log('go back');
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
