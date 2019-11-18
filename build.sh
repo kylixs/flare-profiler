d9d90dc45ee8a0daf14bf8aa3a38c34ecc8877d4
龚德伟 <gongdewei@szjlc.com>
2019-11-18 18:51:33 +0800

    package files after build

--- build.sh
+++ build.sh
@@ -15,3 +15,18 @@ $PROJECT_PATH/build/build-agent.sh $@
 #$PROJECT_PATH/build/build-ui.sh $@
 
 echo "build flare-profiler finished: $PROJECT_PATH/target/flare-profiler"
+
+#package
+echo "packaging ..."
+cd $PROJECT_PATH/target
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    tar --exclude=flare-samples --exclude=*.bat -czf flare-profiler-linux.tar.gz flare-profiler
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    tar --exclude=flare-samples --exclude=*.bat -czf flare-profiler-macos.tar.gz flare-profiler
+elif [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" ]]; then
+    tar --exclude=flare-samples --exclude=*.sh -czf flare-profiler-windows.tar.gz  flare-profiler
+else
+    echo "Unknown OS type: $OSTYPE, ignore packaging."
+fi
+cd $PROJECT_PATH
+echo "packaged: $(find $PROJECT_PATH/target -name flare-profiler-*.tar.gz)"
