950b75b47883587563fc76740e147ee70395f7ee
gongdewei <kylixs@qq.com>
2019-11-23 19:53:58 +0800

    fix  start-flare-server.sh

--- start-flare-server.sh
+++ start-flare-server.sh
@@ -3,6 +3,10 @@
 PROJECT_PATH="$(cd "$(dirname $0)"; pwd -P )"
 
 cd $PROJECT_PATH/flare-server
-./target/x86_64-pc-windows-msvc/release/flare_server.exe
+if [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" ]]; then
+    ./target/x86_64-pc-windows-msvc/release/flare_server.exe
+else
+    ./target/release/flare_server 
+fi
 
 cd $PROJECT_PATH
