fb2897e11da89bfdc59d8a0775cf484ebd7674e2
gongdewei <kylixs@qq.com>
2019-11-26 16:04:28 +0800

    fix agent sh script

--- assets/agent/bin/start-agent-as-user.sh
+++ assets/agent/bin/start-agent-as-user.sh
@@ -19,23 +19,23 @@ if [[ "$USER_ACCOUNT" == "" ]];then
 fi
 shift;
 
-TARGET_PID="$1"
-if [[ "$TARGET_PID" -eq "" ]];then
-    echo "required target java pid"
-    goto usage
-fi
-shift;
+#TARGET_PID="$1"
+#if [[ "$TARGET_PID" -eq "" ]];then
+#    echo "required target java pid"
+#    goto usage
+#fi
+#shift;
 
 ACTION_SCRIPT="start-agent.sh"
 
 #may not have permission to create log dir
-#/usr/bin/su - $USER_ACCOUNT  -s /bin/sh -c "$PROJECT_PATH/bin/$ACTION_SCRIPT $TARGET_PID $@ > $PROJECT_PATH/logs/flare_agent_stdout.log 2>&1  & "
-/usr/bin/su - $USER_ACCOUNT  -s /bin/sh -c "$PROJECT_PATH/bin/$ACTION_SCRIPT $TARGET_PID $@ & "
+#/usr/bin/su - $USER_ACCOUNT  -s /bin/sh -c "$PROJECT_PATH/bin/$ACTION_SCRIPT $* > $PROJECT_PATH/logs/flare_agent_stdout.log 2>&1  & "
+/usr/bin/su - $USER_ACCOUNT  -s /bin/sh -c "$PROJECT_PATH/bin/$ACTION_SCRIPT $* & "
 goto end
 
 
 # usage: #
-echo "Usage: $0 <pid> [options]"
+echo "Usage: $0 <username> <pid> [options]"
 echo "Options:"
 echo "     -interval <sample interval>    # sample interval(ms), default value is 5"
 echo "     -address  <agent address>      # agent bind address, default value is 0.0.0.0:3333"
