470b2a5d3182b06c4e6c0a1a1f3d9f95820e0c25
龚德伟 <gongdewei@szjlc.com>
2019-11-26 15:44:33 +0800

    #20 fix agent script

--- assets/agent/bin/start-agent-as-user.sh
+++ assets/agent/bin/start-agent-as-user.sh
@@ -2,19 +2,27 @@
 
 PROJECT_PATH="$(cd "$(dirname $0)/.."; pwd -P )"
 
+# GOTO for bash, based upon https://stackoverflow.com/a/31269848/5353461
+function goto
+{
+ local label=$1
+ cmd=$(sed -En "/^[[:space:]]*#[[:space:]]*$label:[[:space:]]*#/{:a;n;p;ba};" "$0")
+ eval "$cmd"
+ exit
+}
+
+
 USER_ACCOUNT="$1"
 if [[ "$USER_ACCOUNT" == "" ]];then
     echo "required username"
-    echo "usage: $0 <username> <pid> [options]"
-    exit 1
+    goto usage
 fi
 shift;
 
 TARGET_PID="$1"
 if [[ "$TARGET_PID" -eq "" ]];then
     echo "required target java pid"
-    echo "usage: $0 <username> <pid> [options]"
-    exit 1
+    goto usage
 fi
 shift;
 
@@ -23,3 +31,14 @@ ACTION_SCRIPT="start-agent.sh"
 #may not have permission to create log dir
 #/usr/bin/su - $USER_ACCOUNT  -s /bin/sh -c "$PROJECT_PATH/bin/$ACTION_SCRIPT $TARGET_PID $@ > $PROJECT_PATH/logs/flare_agent_stdout.log 2>&1  & "
 /usr/bin/su - $USER_ACCOUNT  -s /bin/sh -c "$PROJECT_PATH/bin/$ACTION_SCRIPT $TARGET_PID $@ & "
+goto end
+
+
+# usage: #
+echo "Usage: $0 <pid> [options]"
+echo "Options:"
+echo "     -interval <sample interval>    # sample interval(ms), default value is 5"
+echo "     -address  <agent address>      # agent bind address, default value is 0.0.0.0:3333"
+exit 1
+
+# end: #
